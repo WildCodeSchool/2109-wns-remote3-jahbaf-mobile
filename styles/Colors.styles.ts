6f5f82b229570c16b90fcf7d5c0a79a1b9ad46cc
BrianBTI <bertili.brian@gmail.com>
2022-02-21 12:30:39 +0100

    feat: 💄  display projects

--- styles/Colors.styles.ts
+++ styles/Colors.styles.ts
@@ -1,4 +1,6 @@
 export const BACKGROUND_COLOR_LIGHT = "#ddd";
 export const BACKGROUND_COLOR_DARK = "#243353";
 export const TEXT_COLOR_DARK = "#222";
+export const DARK_COLOR_DARKER = "#1B2335";
 export const TEXT_COLOR_LIGHT = "#eee";
+export const INTERACTION_COLOR = "#D7722F";
