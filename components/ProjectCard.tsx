b4df8370c57cb55f66eba66ab6997fa5a36cb214
BrianBTI <bertili.brian@gmail.com>
2022-02-22 15:39:24 +0100

    🚧  Project Detail

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,13 +1,86 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Project as ProjectInterface } from '../models';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "@expo/vector-icons/Ionicons";
 
-export const ProjectCard = ({id, name, description}: ProjectInterface) => {
-    return (
-        <View>
-            <Text>Id: {id}</Text>
-            <Text>Name: {name}</Text>
-            <Text>Decription: {description}</Text>
-        </View>
-    );
-}
+import {
+  DARK_COLOR_DARKER,
+  INTERACTION_COLOR,
+  TEXT_COLOR_LIGHT,
+} from "../styles/index";
+import { Project as ProjectInterface } from "../models";
+
+const footerBtns = [
+  {
+    name: "edit",
+    icon: "create",
+    function: () => {
+      console.log("edit");
+    },
+  },
+  {
+    name: "delete",
+    icon: "trash",
+    function: () => {
+      console.log("delete");
+    },
+  },
+];
+
+export const ProjectCard = ({ id, name, description }: ProjectInterface) => {
+  return (
+    <View style={styles.projectCard}>
+      <Text
+        style={{ fontSize: 30, color: INTERACTION_COLOR, textAlign: "center" }}
+      >
+        {name}
+      </Text>
+      <View style={{ marginTop: 35 }}>
+        <Text style={{ fontSize: 20, color: INTERACTION_COLOR }}>
+          Description
+        </Text>
+        <Text style={{ fontSize: 16, color: TEXT_COLOR_LIGHT, marginTop: 10 }}>
+          {description}
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 40,
+        }}
+      >
+        {footerBtns.map((btn) => (
+          <TouchableOpacity
+            style={{
+              borderRadius: 10,
+              padding: 5,
+              backgroundColor: INTERACTION_COLOR,
+              marginHorizontal: 10,
+            }}
+            onPress={btn.function}
+          >
+            <Icon name={btn.icon as any} color={TEXT_COLOR_LIGHT} size={23} />
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  projectCard: {
+    marginTop: 15,
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 25,
+    width: "96%",
+    alignSelf: "center",
+    backgroundColor: DARK_COLOR_DARKER,
+    shadowColor: DARK_COLOR_DARKER,
+    shadowOffset: { width: 6, height: 6 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
+});
